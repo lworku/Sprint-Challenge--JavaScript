61a8063bcc953d3f8cde23e466df6d5784901f46
lworku <lydiasgthb@gmail.com>
2019-08-16 20:21:50 -0700

    sprint mvp

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,6 +7,7 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+const consume = (x, y, cb) => cb(x, y);
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -14,29 +15,36 @@
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+const add = (x, y) => x + y;
+const multiply = (x, y) => x * y;
+const greeting = (firstName, lastName) => `Hello ${firstName} ${lastName}, nice to meet you!`;
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+consume(2,2,add); // 4
+consume(10,16,multiply); // 160
+consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+
+console.log (consume(2,2,add));
+console.log(consume(10,16,multiply));
+console.log(consume("Mary","Poppins", greeting));
 
 
 // ==== Closures ==== 
 
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
-// Explanation: 
+// Explanation: nestedFunction() is within the block scope of the myFunction() allowing it(nestedFunction()) access to myFunction()'s variables.
 
 
-const external = "I'm outside the function";
+// const external = "I'm outside the function";
 
-function myFunction() {
-  console.log(external);
-  const internal = "Hello! I'm inside myFunction!";
+// function myFunction() {
+//   console.log(external);
+//   const internal = "Hello! I'm inside myFunction!";
 
-  function nestedFunction() {
-    console.log(internal);
-  };
-  nestedFunction();
-}
-myFunction();
+//   function nestedFunction() {
+//     console.log(internal);
+//   };
+//   nestedFunction();
+// }
+// myFunction();
